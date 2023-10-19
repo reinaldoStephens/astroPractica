e929c50e1ff582691fc7a38e16bade37682272ed
Reinaldo Stephens <reinaldo9519@gmail.com>
2023-10-18 21:34:02 -0600

    Api and productCard component

---
+++ src/types/api.ts
@@ -0,0 +1,23 @@
+export type APIFakeStoreProductsResponse = Product[];
+
+export type Product = {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: Category;
+    image: string;
+    rating: Rating;
+};
+
+export enum Category {
+    Electronics = "electronics",
+    Jewelery = "jewelery",
+    MenSClothing = "men's clothing",
+    WomenSClothing = "women's clothing",
+}
+
+export type Rating = {
+    rate: number;
+    count: number;
+};
