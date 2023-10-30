e1a39f4ceefd8119b09375281692bd5e13b66d59
Reinaldo Stephens <reinaldo9519@gmail.com>
2023-10-30 17:25:16 -0600

    adding contact page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        viewTransitions: true,
-    },
+    integrations: [react()],
+    viewTransitions: true,
 });
